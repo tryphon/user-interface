8eb5c0b92bd5f3d5d19768b0e6d1df1315f8436c
Alban Peignier <alban.peignier@free.fr>
2009-12-27 16:25:14 +0100

    Create a Rails plugin

---
+++ lib/user_interface.rb
@@ -0,0 +1,14 @@
+# UserInterface
+
+module UserInterface
+  
+end
+
+require 'user_interface/helpers/flash_helper'
+require 'user_interface/helpers/title_helper'
+
+if defined?(ActionView::Base)
+  ActionView::Base.send :include, UserInterface::FlashHelper
+  ActionView::Base.send :include, UserInterface::TitleHelper
+end
+
