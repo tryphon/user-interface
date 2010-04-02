95677334c1c515aeb29d14d14b79063705068067
Alban Peignier <alban.peignier@free.fr>
2010-04-02 18:48:56 +0200

    Create WillPaginerHelper from Pige project

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -7,9 +7,14 @@ end
 require 'user_interface/locale_management'
 require 'user_interface/helpers/flash_helper'
 require 'user_interface/helpers/title_helper'
+require 'user_interface/helpers/will_paginate_helper'
 
 if defined?(ActionView::Base)
   ActionView::Base.send :include, UserInterface::FlashHelper
   ActionView::Base.send :include, UserInterface::TitleHelper
+end
+
+if defined?(WillPaginate::ViewHelpers)
+  ActionView::Base.send :include, UserInterface::WillPaginateHelper
 end
 
