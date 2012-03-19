8097157a795d2ddf69405aed70dd3df6b3ae3d81
Alban Peignier <alban@tryphon.eu>
2012-03-19 19:44:24 +0100

    Fixes flash image paths and helper

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -5,7 +5,7 @@ module FlashHelper
       content_tag(:div, :id => "flash") do
         flash.collect do |level, message|
           content_tag :div, sanitize(message), :class => level
-        end.join("\n")
+        end.join("\n").html_safe
       end
     end
   end
