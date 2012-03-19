462139b265e0540677dc4d855aa4e19200e6745e
Alban Peignier <alban@tryphon.eu>
2012-03-19 19:45:23 +0100

    Provides (first version of) i18n devise views

--- lib/user_interface/engine.rb
+++ lib/user_interface/engine.rb
@@ -6,6 +6,10 @@ module UserInterface
     config.to_prepare do
       ApplicationController.send :include, UserInterface::UserSessionManagement
       ApplicationController.send :include, UserInterface::LocaleManagement
+
+      # To use our devise views instead of original ones
+      ApplicationController.prepend_view_path File.expand_path("../../../app/views", __FILE__)
+      ActionMailer::Base.prepend_view_path File.expand_path("../../../app/views", __FILE__)
     end
   end
 end
