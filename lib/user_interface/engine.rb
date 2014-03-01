31b30ac8d9a936d43c22bc92e8ee9323da570d44
Alban Peignier <alban@tryphon.eu>
2014-03-01 12:52:23 +0100

    Include config/locales/**.yml when engine is initialized

--- lib/user_interface/engine.rb
+++ lib/user_interface/engine.rb
@@ -10,7 +10,7 @@ module UserInterface
       # To use our devise views instead of original ones
 
       view_path = File.expand_path("../../../app/views", __FILE__)
-      
+
       devise_position = ApplicationController.view_paths.map(&:to_s).index { |p| p.match /devise/ }
       if devise_position
         ApplicationController.view_paths.insert devise_position, view_path
@@ -20,5 +20,7 @@ module UserInterface
 
       ActionMailer::Base.prepend_view_path view_path
     end
+
+    config.i18n.load_path += Dir[File.join(File.expand_path("../../..", __FILE__), 'config', 'locales', '**', '*.yml')]
   end
 end
